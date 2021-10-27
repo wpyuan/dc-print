24e81a9de63986287a71d29d6f3dabb555f734bb
wangpeiyuan <490176245@qq.com>
2021-10-27 17:24:19 +0800

    优化 依赖

--- src/main/java/com/github/dc/print/config/PrintHandlerAutoConfiguration.java
+++ src/main/java/com/github/dc/print/config/PrintHandlerAutoConfiguration.java
@@ -18,7 +18,7 @@ import java.util.Map;
  * @author PeiYuan
  */
 @Configuration
-@DependsOn("applicationContextHelper")
+@DependsOn("dcPrintApplicationContextHelper")
 public class PrintHandlerAutoConfiguration {
 
     @Bean
